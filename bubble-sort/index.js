6b21288a4cf147fc9b0d3f62857527a9e20e3bba
Jean Carlo Nascimento <jnascimento@gmail.com>
2016-11-12 07:24:19 -0200

    Update index.js

--- bubble-sort/index.js
+++ bubble-sort/index.js
@@ -1,24 +1,25 @@
-var copy = require('../util/copy')
-var swap = require('../util/swap')
+const copy = require('../util/copy')
+const swap = require('../util/swap')
+const isBigger = (current, next) => current > next
 
 module.exports = function (list) {
-  var array = copy(list)
-  var size = array.length
-  var currentPosition, nextPosition
-  var swapped
+  let array = copy(list)
+  const size = array.length
+  let currentPosition = 0
+  let nextPosition = 0
+  let swapped = false
 
-  do {
-    swapped = false
+  while (!swapped) {
     for (currentPosition = 0; currentPosition < size - 2; currentPosition += 1) {
 
       nextPosition = currentPosition + 1
 
-      if (array[currentPosition] > array[nextPosition]) {
+      if (isBigger(array[currentPosition], array[nextPosition])) {
         swap(array, currentPosition, nextPosition)
         swapped = true
       }
     }
-  } while (swapped)
+  } 
   
   return array
 }
